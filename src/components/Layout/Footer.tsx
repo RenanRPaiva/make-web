cdc4cc8663af3f83b1f1f06f8761830ea0dd0587
Renan <renan.paiva@al.infnet.edu.br>
2022-03-26 19:25:13 -0300

    Implementa Footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,34 +2,29 @@ import { faFacebookSquare, faInstagram, faWhatsappSquare } from "@fortawesome/fr
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Container, Nav } from "react-bootstrap"
 import styled from "styled-components"
-import LogoWhite from "../../assets/img/logo-make-white.svg"
 import LogoWhiteNis from "../../assets/img/logo-white-full.svg"
 
 export default function Footer() {
     return (
-        <FooterStyled className="text-center">
-            <Container>
-                <div className="d-flex align-items-center">
-                    <img src={LogoWhite} alt='Make+' width={53} height={49} />
-                    <LogoStyled className="text-white p-2">Make+</LogoStyled>
-                </div>
-                <p className="text-white">Oferecidor por:</p>
-                <img src={LogoWhiteNis} alt='Nislaine Duarte - Beauty professional' width={177} height={88} />
-                <Nav>
-                    <Nav.Link href="/">Início</Nav.Link>
-                    <Nav.Link href="/cadastro">Cadastro</Nav.Link>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link href="/termos-de-uso.pdf" target='_blank'>Termos de Uso</Nav.Link>
+        <FooterStyled className="text-center pt-4 pb-5 py-lg-3">
+            <Container className="d-lg-flex align-items-center">
+                    <p className="text-white">Oferecidor por:</p>
+                    <img src={LogoWhiteNis} alt='Nislaine Duarte - Beauty professional' width={177} height={88} />             
+                <Nav className="justify-content-center my-4 my-lg-0 ms-auto">
+                    <Nav.Link href="/" className="text-white">Início</Nav.Link>
+                    <Nav.Link href="/cadastro" className="text-white">Cadastro</Nav.Link>
+                    <Nav.Link href="/login" className="text-white">Login</Nav.Link>
+                    <Nav.Link href="/termos-de-uso.pdf" target='_blank' className="text-white">Termos de Uso</Nav.Link>
                 </Nav>
-                <Nav>
-                    <Nav.Link href="https://www.facebook.com/nislainend" target='_blank' rel='noreferrer noopener'>
-                        <FontAwesomeIcon icon={faFacebookSquare} />
+                <Nav className="justify-content-center">
+                    <Nav.Link href="https://www.facebook.com/nislainend" target='_blank' rel='noreferrer noopener' className="py-0 px-2">
+                        <IconStyled icon={faFacebookSquare} className="text-white"/>
                     </Nav.Link>
-                    <Nav.Link href="https://www.instagram.com/nislaineduartemakeup/" target='_blank' rel='noreferrer noopener'>
-                        <FontAwesomeIcon icon={faInstagram} />
+                    <Nav.Link href="https://www.instagram.com/nislaineduartemakeup/" target='_blank' rel='noreferrer noopener' className="py-0 px-2">
+                        <IconStyled icon={faInstagram} className="text-white"/>
                     </Nav.Link>
-                    <Nav.Link href="https://web.whatsapp.com/send/?phone=5521996443691&text=Ol%C3%A1,%20gostaria%20de%20fazer%20um%20pedido!" target='_blank' rel='noreferrer noopener'>
-                        <FontAwesomeIcon icon={faWhatsappSquare} />
+                    <Nav.Link href="https://web.whatsapp.com/send/?phone=5521996443691&text=Ol%C3%A1,%20gostaria%20de%20fazer%20um%20pedido!" target='_blank' rel='noreferrer noopener' className="py-0 px-2">
+                        <IconStyled icon={faWhatsappSquare} className="text-white"/>
                     </Nav.Link>
                 </Nav>
             </Container>
@@ -40,7 +35,6 @@ export default function Footer() {
 const FooterStyled = styled.footer`
     background-color: #414141;
 `
-const LogoStyled = styled.p`
-    font-family: 'Nova Oval', cursive; 
-    margin-top: 15px;
+const IconStyled = styled(FontAwesomeIcon)`
+    font-size: 2.5rem;
 `
