13547268cedfc1efded504465df1d7f703be44d1
Renan <renan.paiva@al.infnet.edu.br>
2022-04-19 22:59:25 -0300

    Adiciona componente FormField

---
+++ src/components/FormField/index.tsx
@@ -0,0 +1,32 @@
+import { Form, FormControlProps } from "react-bootstrap";
+import { IMaskInput } from "react-imask";
+
+type Props = {
+    label?: string
+    controlId?: string
+    error?: string
+    mask?: { mask: string }[]
+    onAccept?: (value: unknown) => void
+} & FormControlProps
+
+export default function FormField ({ label, controlId, error, mask, onAccept, ...otherProps }:Props) {
+    return (
+        <Form.Group className="mb-3" controlId={controlId}>
+            {label && <Form.Label className="mb-1">{label}</Form.Label>}
+            {mask ? (
+                <Form.Control 
+                {...otherProps} 
+                as={IMaskInput} 
+                mask={mask}
+                onChange={undefined}
+                onAccept={onAccept}
+                />
+            ) : (
+                <Form.Control {...otherProps}/>
+            )}
+            <Form.Control.Feedback type='invalid'>
+                {error}
+            </Form.Control.Feedback>
+        </Form.Group>
+    )
+}
