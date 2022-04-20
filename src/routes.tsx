a5a73f494adbc26547c55a85bf23a64a8cc9fa22
Renan <renan.paiva@al.infnet.edu.br>
2022-04-19 21:48:05 -0300

    Adiciona página de cadastro

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes as RoutesRD } from 'react-router-dom'
 import HomeView from './views/Home'
 import { NotFoundView } from './views/NotFound'
+import RegisterView from './views/Register'
 
 export default function Routes () {
     return (
         <RoutesRD>
             <Route path='/' element={<HomeView />} />
+            <Route path='/cadastro' element={<RegisterView />} />
             <Route path='*' element={<NotFoundView />} />
         </RoutesRD>
     )
---
+++ src/views/Register/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout"
+import PageTitle from "../../components/PageTitle"
+
+export default function RegisterView() {
+    return (
+        <Layout>
+            <PageTitle>Nova conta</PageTitle>
+        </Layout>
+    )
+}
