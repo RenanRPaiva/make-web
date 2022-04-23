2ac8220acc12fd68a1583d204a03f8d9e97bb217
Renan <renan.paiva@al.infnet.edu.br>
2022-04-23 17:05:05 -0300

    inicia estrutura da tela de cadastro

--- src/assets/css/global.ts
+++ src/assets/css/global.ts
@@ -4,4 +4,8 @@ export const GlobalStyle = createGlobalStyle`
    body {
     font-family: 'Lato', sans-serif;
    }
+   a {
+      color: #BD8085;
+      text-decoration: none;
+   }
 `
--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -1,10 +1,59 @@
+import { Col, Container, Form, Row } from "react-bootstrap"
+import FormField from "../../components/FormField"
 import Layout from "../../components/Layout"
 import PageTitle from "../../components/PageTitle"
 
 export default function RegisterView() {
     return (
         <Layout>
-            <PageTitle>Nova conta</PageTitle>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col lg={4}>
+                        <PageTitle>Nova conta</PageTitle>
+                        <FormField
+                            label='Nome'
+                            placeholder="Digite seu nome"
+                            controlId="input-name"
+                            isInvalid
+                            error="Preencha o nome"
+                        />
+                        <FormField
+                            label='E-mail'
+                            placeholder="Ele será o seu usuário "
+                            controlId="input-email"
+                            isInvalid
+                            error="Preencha seu e-mail"
+                        />
+                        <FormField
+                            label='Telefone'
+                            placeholder="(00) 00000-0000"
+                            controlId="input-phone"
+                            isInvalid
+                            error="Preencha seu telefone"
+                            type="fone"
+                            mask={[
+                                { mask: '(00) 0000-0000' },
+                                { mask: '(00) 00000-0000' }
+                            ]}
+                        />
+                        <FormField
+                            label='Senha'
+                            placeholder="Informe sua senha de acesso"
+                            controlId="input-password"
+                            isInvalid
+                            error="Preencha sua senha"
+                            type="password"
+                        />
+                        <Form.Group className="mb-3" controlId="input-agree">
+                            <Form.Check
+                                type="checkbox"
+                                label={<span>Eu li e aceito os <a href='/termos-de-uso.pdf' target='_blank'>Termos de Uso</a>.</span>}
+                            />
+                        </Form.Group>
+
+                    </Col>
+                </Row>
+            </Container>
         </Layout>
     )
 }
