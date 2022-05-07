d8099184bf7d88ba276bc5fbc29c975b1343666c
Renan <renan.paiva@al.infnet.edu.br>
2022-05-06 21:40:18 -0300

    Instala firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrWmRYVROoc77HEnkcRuUUXh8gLCdXLjA",
+  authDomain: "make-f23f1.firebaseapp.com",
+  projectId: "make-f23f1",
+  storageBucket: "make-f23f1.appspot.com",
+  messagingSenderId: "1063484544496",
+  appId: "1:1063484544496:web:30dd2c705463f01725f935"
+};
+
+const app = initializeApp(firebaseConfig);
