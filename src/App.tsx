e758b9e4eaa95db4c4e53d3a422e5a12891324ab
Renan <renan.paiva@al.infnet.edu.br>
2022-05-23 21:47:28 -0300

    Identifica que usuário está logado ao carregar app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useEffect } from "react";
 import Routes from "./routes";
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "./services/firebase";
+import { getUser } from "./services/getUser";
+import { useDispatch } from "react-redux";
+import { updateUser } from "./store/slices/userSlice";
 
 function App() {
+  const dispatch = useDispatch()
   useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      console.log('currentUser', currentUser)
+    onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const user = await getUser(currentUser.uid)
+        dispatch(updateUser(user))
+      }
     })
-  }, [])
+  }, [dispatch])
   return (
    <Routes />
   );
