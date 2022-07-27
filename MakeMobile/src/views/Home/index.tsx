624b56b9c27667627b03d195118f7c4969087ed6
Renan <renan.paiva@al.infnet.edu.br>
2022-07-26 21:00:47 -0300

    Corrigi erro de modulos

--- MakeMobile/src/views/Home/index.tsx
+++ MakeMobile/src/views/Home/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {ImageBackground, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Image, ImageBackground, SafeAreaView, StyleSheet, Text} from 'react-native';
 import bg from '../../assets/img/bg-make-mobile.jpg';
 
 export function HomeView() {
   return (
     <ImageBackground source={bg} style={styles.background}>
       <SafeAreaView style={styles.view}>
-        <View >
-          <Text>Logo</Text>
-          <Text>Make +</Text>
-        </View>
+        <Image source={require('../../assets/img/logo-make-white.svg')} />
+        <Text>Make +</Text>
         <Text>Faça login e escolha os pedidos!</Text>
         <Text>Fazer login</Text>
       </SafeAreaView>
