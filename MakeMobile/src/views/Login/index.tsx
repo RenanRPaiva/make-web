f4651f03203056147a53a337dd0cf37f42360ada
Renan <renan.paiva@al.infnet.edu.br>
2022-07-28 15:45:16 -0300

    Implementa estrutura do formulário

--- MakeMobile/src/views/Login/index.tsx
+++ MakeMobile/src/views/Login/index.tsx
@@ -1,11 +1,60 @@
+import {useFormik} from 'formik';
 import React from 'react';
-import {Text} from 'react-native';
 import {Container} from '../../components/Container';
+import {CustomButton} from '../../components/CustomButton';
+import {FormField} from '../../components/FormField';
+import * as yup from 'yup';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 export function LoginView() {
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: yup.object().shape({
+      email: yup
+        .string()
+        .required('Informe o e-mail.')
+        .email('Informe um e-mail válido.'),
+      password: yup.string().required('Informe a senha.'),
+    }),
+    onSubmit: async values => {
+      console.log('values', values);
+    },
+  });
+  const getFieldProps = (fieldName: keyof FormValues) => ({
+    value: formik.values[fieldName],
+    onChangeText: formik.handleChange(fieldName),
+    onBlur: formik.handleBlur(fieldName),
+    error: formik.errors[fieldName],
+    isInvalid: formik.touched[fieldName] && !!formik.errors[fieldName],
+    isValid: formik.touched[fieldName] && !formik.errors[fieldName],
+  });
   return (
     <Container padding>
-      <Text>Aqui vai ser o login</Text>
+      <FormField
+        {...getFieldProps('email')}
+        label="E-mail"
+        placeholder="Informe seu e-mail de acesso"
+        keyboardType="email-address"
+      />
+      <FormField
+        {...getFieldProps('password')}
+        label="Senha"
+        placeholder="Informe sua senha de acesso"
+        secureTextEntry
+      />
+      <CustomButton
+        onPress={formik.handleSubmit}
+        disabled={formik.isValidating || formik.isSubmitting}
+        loading={formik.isValidating || formik.isSubmitting}>
+        Entrar
+      </CustomButton>
     </Container>
   );
 }
