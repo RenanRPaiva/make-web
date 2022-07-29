a7dd69c5e76f2f980eaac701a00b7669ce2bd6e0
Renan <renan.paiva@al.infnet.edu.br>
2022-07-28 23:56:11 -0300

    Personaliza botão de logout

--- MakeMobile/src/components/LogoutButton/index.tsx
+++ MakeMobile/src/components/LogoutButton/index.tsx
@@ -1,10 +1,37 @@
+import {faRightFromBracket} from '@fortawesome/free-solid-svg-icons/faRightFromBracket';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import {Button} from 'react-native';
+import {StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import {logoutUser} from '../../services/logoutUser';
 
 export function LogoutButton() {
   const handleLogout = async () => {
-    await logoutUser();
+    Alert.alert(
+      'Tem certeza que deseja sair?',
+      'Você precisará informar o e-mail e senha no próximo acesso.',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'ok',
+          onPress: async () => {
+            await logoutUser();
+          },
+        },
+      ],
+    );
   };
-  return <Button title="sair" onPress={handleLogout} />;
+  return (
+    <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
+      <FontAwesomeIcon icon={faRightFromBracket} color="#FFF" size={25} />
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  logoutButton: {
+    padding: 10,
+  },
+});
